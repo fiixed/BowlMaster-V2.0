3c0baa5437e138e605f3538ce4d73af1bea8e5dc
Andrew Bell <fiixed@gmail.com>
2016-09-10 13:12:30 -0500

    added ball and ball rolling script

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public float launchSpeed;
+
+	private Rigidbody rb;
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Space)) {
+			Launch();
+		}
+	}
+
+	void Launch() {
+		rb.velocity = new Vector3(0, 0, launchSpeed);
+			audioSource.Play();
+	}
+}
